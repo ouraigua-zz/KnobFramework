afb885f87df55510392abc313d6e385015e2e494
Jalal Ouraigua <6299112+ouraigua@users.noreply.github.com>
2018-12-08 17:47:31 +0000

    Initial Commit

---
+++ KnobFramework/KnobFramework.h
@@ -0,0 +1,19 @@
+//
+//  KnobFramework.h
+//  KnobFramework
+//
+//  Created by Jalal Ouraigua on 08/12/2018.
+//  Copyright © 2018 Jalal Ouraigua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobFramework.
+FOUNDATION_EXPORT double KnobFrameworkVersionNumber;
+
+//! Project version string for KnobFramework.
+FOUNDATION_EXPORT const unsigned char KnobFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobFramework/PublicHeader.h>
+
+
